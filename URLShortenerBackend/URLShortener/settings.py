b0920f962ca0e141f697e6b162954b6bd584a0b3
Your Name <you@example.com>
2022-06-28 08:55:22 +0800

    unify env name

--- URLShortenerBackend/URLShortener/settings.py
+++ URLShortenerBackend/URLShortener/settings.py
@@ -85,9 +85,9 @@ WSGI_APPLICATION = 'URLShortener.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'NAME': os.getenv('MYSQL_DATABASE'),
+        'USER': os.getenv('MYSQL_USER'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': int(os.getenv('DB_PORT')),
     }
